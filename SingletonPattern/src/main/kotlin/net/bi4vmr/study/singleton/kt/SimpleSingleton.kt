d013f5db8e73e22f415237b8922ea1fb5d71789b
BI4VMR <bi4vmr@outlook.com>
2023-10-02 12:54:43 +0800

    C740808E-EA99-4507-8F54-0E08625E3435

---
+++ SingletonPattern/src/main/kotlin/net/bi4vmr/study/singleton/kt/SimpleSingleton.kt
@@ -0,0 +1,21 @@
+package net.bi4vmr.study.singleton.kt
+
+/**
+ * Name        : Singleton
+ *
+ * Author      : BI4VMR
+ *
+ * Email       : bi4vmr@outlook.com
+ *
+ * Date        : 2023-10-01 13:38
+ *
+ * Description : 单例模式 - 饿汉式。
+ */
+object SimpleSingleton {
+
+    // 业务方法
+    fun function() {
+        // 其他功能...
+        println("执行方法...")
+    }
+}
